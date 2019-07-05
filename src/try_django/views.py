2d5482cc4a76ee42eb958004eac899ea0c1f81ec
itzamnahuerta <huerta.ek@gmail.com>
2019-07-05 14:40:26 -0400

    first template file: helloworld.html

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1> hello world </h1>")
+    return render(request, "hello_world.html")
 
 def about_page(request):
     return HttpResponse("<h1> about us </h1>")
