88e1080f35ff37beff2b0bf006211b3e7e8693a2
itzamnahuerta <huerta.ek@gmail.com>
2019-07-07 14:52:07 -0400

    create first app (blog)

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 def home_page(request):
-    title = "hello there ...  "
-    return render(request, "hello_world.html", {"title":title})
+    title = " Welcome "
+    context = {"title": title}
+    if request.user.is_authenticated:
+        context = {"title": title, "my_list": [1,2,3,4,5,6]}
+    return render(request, "home.html", context)
 
 def about_page(request):
     return render(request, "about.html", {"title": "About Us"})
@@ -13,8 +16,8 @@ def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
 
 def example_page(request):
-    context = {"title": "Example"}
-    template_name = "hello_world.html"
+    context         = {"title": "Example"}
+    template_name   = "hello_world.html"
     template_object = get_template(template_name)
     return HttpResponse(template_object.render(context))
         
