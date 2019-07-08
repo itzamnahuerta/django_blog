cb53187be9e9bbc1b6e076a7c76254b458635a0e
itzamnahuerta <huerta.ek@gmail.com>
2019-07-08 13:02:29 -0400

    a new database lookup value

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,16 +1,11 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request, post_id):
-    try: 
-        obj = BlogPost.objects.get(id=post_id)
-    except BlogPost.DoesNotExsist:
-        raise Http404
-    except ValueError:
-        raise Http404 
+def blog_post_detail_page(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail_page.html'
     context = {"object": obj} 
     return render(request, template_name, context)
