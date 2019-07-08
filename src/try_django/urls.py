47323166ea9b8a1c0e60969ff77f070f191d2ad3
itzamnahuerta <huerta.ek@gmail.com>
2019-07-08 12:17:00 -0400

    model in a view

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path #regular expression url 
+from blog.views import (
+    blog_post_detail_page,
+)
+
 from .views import (
     home_page,
     about_page,
@@ -24,6 +28,7 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
+    path('blog/', blog_post_detail_page),
     re_path(r'pages?/$', about_page),
     re_path(r'about/$', about_page),
         path('example/', example_page),
