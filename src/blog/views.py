4fc4e350daf37033d35ca030ebb20c6f9bd463e3
itzamnahuerta <huerta.ek@gmail.com>
2019-07-08 13:17:31 -0400

    a unique slug

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,9 +7,9 @@ from .models import BlogPost
 def blog_post_detail_page(request, slug):
     # obj = get_object_or_404(BlogPost, slug=slug)
     queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
-    
+    if queryset.count() == 0:
+        raise Http404
+    obj = queryset.first()
     template_name = 'blog_post_detail_page.html'
     context = {"object": obj} 
     return render(request, template_name, context)
