911a88b6c875d3ae665c9fc60535e2ff173d69e2
itzamnahuerta <huerta.ek@gmail.com>
2019-07-10 20:46:18 -0400

    crud & view templates

--- src/blog/views.py
+++ src/blog/views.py
@@ -9,3 +9,24 @@ def blog_post_detail_page(request, slug):
     template_name = 'blog_post_detail_page.html'
     context = {"object": obj} 
     return render(request, template_name, context)
+
+
+# GET = RETRIVE/LIST
+# POST = CREATE/UPDATE/DELETE
+
+def blog_post_list_view(request):
+    return 
+
+def blog_post_create_view(request):
+    return 
+
+def blog_post_retrieve_view(request):
+    return 
+
+def blog_post_update_view(request):
+    return 
+
+def blog_post_delete_view(request):
+    return 
+
+    
