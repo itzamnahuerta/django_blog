1812ff16584b88c9271ee04bc713c2167a0d4083
itzamnahuerta <huerta.ek@gmail.com>
2019-07-11 17:09:42 -0400

    routing the views

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,16 +4,8 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail_page.html'
-    context = {"object": obj} 
-    return render(request, template_name, context)
-
-
 # GET = RETRIVE/LIST
 # POST = CREATE/UPDATE/DELETE
-
 def blog_post_list_view(request):
     # list out objects / could be search
     qs = BlogPost.objects.all() # queryset -> list of python objects 
